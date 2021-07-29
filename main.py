ee1a984c848f027a0feca24cb42646d3cf4ad0cc
Wiktor Lasota <37120203+Wojtycha@users.noreply.github.com>
2021-07-29 06:57:42 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class App(tk.Frame):
         self.master = master
         self.pack()
 
-        self.exitButton = ttk.Button(root, text="Wybierz plik", command=self.browseFile)
+        self.exitButton = ttk.Button(root, text="Wybierz plik", command=self.browseFile, width=25)
         self.exitButton.pack()
 
     def browseFile(self):
@@ -19,17 +19,17 @@ class App(tk.Frame):
 
         self.wb = load_workbook(self.file.name)
         self.availableSheets = ttk.Label(root, text="Dostępne arkusze: ")
-        self.availableSheets.pack()
+        self.availableSheets.pack(pady=5)
 
         self.option = StringVar()
-        self.sheetCombo = ttk.Combobox(root, textvariable=self.option, values=self.wb.sheetnames)
+        self.sheetCombo = ttk.Combobox(root, textvariable=self.option, values=self.wb.sheetnames, width=22)
         self.sheetCombo.current(0)
         self.sheetCombo.pack()
 
-        self.changeButton = ttk.Button(root, text="Zamień dane/wypełnij", command=self.manipulateData)
-        self.changeButton.pack()
+        self.changeButton = ttk.Button(root, text="Zamień dane/wypełnij", command=self.manipulateData, width=25)
+        self.changeButton.pack(pady=5)
 
-        self.mergeButton = ttk.Button(root, text="Połącz z drugim plikiem", command=self.mergeWindow)
+        self.mergeButton = ttk.Button(root, text="Połącz z drugim plikiem", command=self.mergeWindow, width=25)
         self.mergeButton.pack()
 
     def manipulateData(self):
@@ -40,17 +40,17 @@ class App(tk.Frame):
         self.ws = self.wb[self.sheetCombo.get()]
 
         self.lbl = ttk.Label(self.newWindow, text="Wartość do wprowadzenia: ")
-        self.usrInput = ttk.Entry(self.newWindow)
-        self.lbl.pack()
+        self.usrInput = ttk.Entry(self.newWindow, width=50)
+        self.lbl.pack(pady=5)
         self.usrInput.pack()
 
         self.cordLabel = ttk.Label(self.newWindow, text="Współrzędne:")
-        self.cordLabel.pack()
+        self.cordLabel.pack(pady=10)
         self.cordEntry = ttk.Entry(self.newWindow)
         self.cordEntry.pack()   
 
-        self.applyButton = ttk.Button(self.newWindow, text="Potwierdź", command=self.apply)
-        self.applyButton.pack()
+        self.applyButton = ttk.Button(self.newWindow, text="Potwierdź", command=self.apply, width=20)
+        self.applyButton.pack(pady=5)
 
     def apply(self):
         coord = self.cordEntry.get()
@@ -70,6 +70,7 @@ class App(tk.Frame):
             self.wb.save(self.file.name)
             self.successLabel = tk.Label(self.newWindow, text="Pomyślnie zapisano dane!", fg='green')
             self.successLabel.pack()
+
         except:
             print("Błąd!")
 
@@ -85,15 +86,15 @@ class App(tk.Frame):
 
         self.wbTwo = load_workbook(self.fileTwo.name)
         self.availableSheetsTwo = ttk.Label(self.newWindow, text="Dostępne arkusze: ")
-        self.availableSheetsTwo.pack()
+        self.availableSheetsTwo.pack(pady=10)
 
         self.optionTwo = StringVar()
         self.sheetComboTwo = ttk.Combobox(self.newWindow, textvariable=self.optionTwo, values=self.wbTwo.sheetnames)
         self.sheetComboTwo.current(0)
         self.sheetComboTwo.pack()
 
-        self.actionButtonTwo = ttk.Button(self.newWindow, text="Wybierz", command=self.merge)
-        self.actionButtonTwo.pack()
+        self.actionButtonTwo = ttk.Button(self.newWindow, text="Połącz dane", command=self.merge, width=22)
+        self.actionButtonTwo.pack(pady=10)
 
     def merge(self):
         self.wsTwo = self.wbTwo[self.sheetComboTwo.get()]
@@ -104,7 +105,12 @@ class App(tk.Frame):
             for j in range(1, self.wsTwo.max_row):
                 self.ws.cell(column=offset+i, row=j, value=self.wsTwo.cell(column=i, row=j).value)
 
-        self.wb.save(self.file.name)
+        try:
+            self.wb.save(self.file.name)
+            self.successLabel = tk.Label(self.newWindow, text="Pomyślnie dołączono dane!", fg='green')
+            self.successLabel.pack()
+        except:
+            print("ERROR")
         
 
 root = tk.Tk()
@@ -113,5 +119,6 @@ myapp = App(root)
 myapp.master.title("Excel multitool")
 root.geometry("400x300+50+50")
 root.resizable(False,False)
+root.columnconfigure(0, weight=1)
 
 myapp.mainloop()
